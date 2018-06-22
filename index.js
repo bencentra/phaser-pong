e86449c8d632ea4e4555318c368b3461f4c12915
Ben Centra <blcentra@gmail.com>
2018-06-22 18:13:27 -0400

    update for phaser3

--- index.js
+++ index.js
@@ -1,122 +1,132 @@
 (function(Phaser) {
+  var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 0 },
+        debug: false
+      }
+    },
+    scene: {
+      preload: preload,
+      create: create,
+      update: update
+    }
+  };
 
-  // Game "actors" and properties
-  var game, paddleL, paddleR, ball;
-  var PADDLE_W = 24;
-  var PADDLE_H = 72;
-  var PADDLE_Vy = 250;
-  var BALL_W = 24;
-  var BALL_Vx = 150;
-  var BALL_Vy = 50;
+  var game = new Phaser.Game(config);
+  var redPaddle; // AI paddle
+  var redScore = 0;
+  var redScoreText;
+  var redPaddleSpeed = 150;
+  var bluePaddle; // Player paddle
+  var blueScore = 0;
+  var blueScoreText;
+  var ball;
+  var cursors;
 
-  // Scores
-  var scoreL = 0;
-  var scoreTextL = '';
-  var scoreR = 0;
-  var scoreTextR = '';
+  var ballDirectionX;
+  var ballDirectionY;
+  var ballSpeedX;
+  var ballSpeedY;
 
-  // Game instance
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
-
-  /*
-  * Preload game assets
-  */
   function preload() {
-    game.load.image('red-paddle', 'assets/red-paddle.png');
-  	game.load.image('blue-paddle', 'assets/blue-paddle.png');
-  	game.load.image('ball', 'assets/ball.png');
+    this.load.image('ball', './assets/ball.png');
+    this.load.image('red-paddle', './assets/red-paddle.png');
+    this.load.image('blue-paddle', './assets/blue-paddle.png');
   }
 
-  /*
-  * Initialize actors and game state
-  */
   function create() {
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-  	paddleL = game.add.sprite(50, (game.world.centerY) - (PADDLE_H / 2), 'red-paddle');
-  	paddleR = game.add.sprite(game.world.width - PADDLE_W - 50, (game.world.centerY) - (PADDLE_H / 2), 'blue-paddle');
-  	ball = game.add.sprite(game.world.centerX, game.world.centerY, 'ball');
+    // Create the sprites
+    redPaddle = this.physics.add.sprite(30, 300, 'red-paddle');
+    redPaddle.setData('name', 'red-paddle');
+    redPaddle.setCollideWorldBounds(true);
+    redPaddle.setImmovable(true);
+    bluePaddle = this.physics.add.sprite(770, 300, 'blue-paddle');
+    bluePaddle.setData('name', 'blue-paddle');
+    bluePaddle.setCollideWorldBounds(true);
+    bluePaddle.setImmovable(true);
+    ball = this.physics.add.sprite(0, 0, 'ball');
+    ball.setCollideWorldBounds(true);
+    resetBall();
+    // Set the ball's physics
+    ball.setBounce(1);
+    ball.setVelocityX(ballSpeedX);
+    ball.setVelocityY(ballSpeedY);
+    // Make the ball collide with the paddles
+    this.physics.add.collider(redPaddle, ball, handleBounce, null, this);
+    this.physics.add.collider(bluePaddle, ball, handleBounce, null, this);
+    // Allow simple arrow key input
+    cursors = this.input.keyboard.createCursorKeys();
+    // Display the scores
+    redScoreText = this.add.text(100, 30, 'Score: ' + redScore, { color: 'red' });
+    blueScoreText = this.add.text(600, 30, 'Score: ' + redScore, { color: 'blue' });
 
-  	game.physics.arcade.enable(paddleL);
-  	paddleL.body.immovable = true;
-  	game.physics.arcade.enable(paddleR);
-  	paddleR.body.immovable = true;
-  	game.physics.arcade.enable(ball);
 
-  	// Initial ball velocity
-  	ball.body.velocity.x = BALL_Vx;
-  	ball.body.velocity.y = BALL_Vy;
-    ball.body.collideWorldBounds = true;
-    ball.body.bounce.set(1);
-
-    // Score text
-    scoreTextL = game.add.text(game.world.centerX - 100, 20, scoreL, { font: '24pt Arial', fill: '#BC0000' });
-    scoreTextR = game.add.text(game.world.centerX + 100, 20, scoreR, { font: '24pt Arial', fill: '#0000BC' });
   }
 
-  /*
-  * Update the game state on each frame
-  */
   function update() {
-    // Move the paddles
-  	handleInput(paddleL, Phaser.Keyboard.W, Phaser.Keyboard.S);
-  	handleInput(paddleR, Phaser.Keyboard.UP, Phaser.Keyboard.DOWN);
-
-  	// Ball collision
-  	if (game.physics.arcade.collide(ball, paddleL)) {
-  		 handleCollision(ball, paddleL);
-  	}
-  	if (game.physics.arcade.collide(ball, paddleR)) {
-  		handleCollision(ball, paddleR);
-  	}
-
-    // Scoring
-    var scored = false;
-    if (ball.body.position.x < 5) {
-      scoreR += 1;
-      scoreTextR.setText(scoreR);
-      scored = 'right';
-    } else if (ball.body.position.x + ball.body.width > game.world.width - 5) {
-      scoreL += 1;
-      scoreTextL.setText(scoreL);
-      scored = 'left';
+    // Move the player
+    if (cursors.up.isDown) {
+      bluePaddle.setVelocityY(-250);
+    } else if (cursors.down.isDown) {
+      bluePaddle.setVelocityY(250);
+    } else {
+      bluePaddle.setVelocityY(0);
     }
-    if (scored) {
-      ball.body.position.x = game.world.centerX;
-      ball.body.position.y = game.world.centerY;
-      ball.body.velocity.x = BALL_Vx;
-    	ball.body.velocity.y = BALL_Vy;
-      if (scored === 'right') ball.body.velocity.x *= -1;
+    // Move the AI
+    if (redPaddle.body.position.y >= ball.body.position.y) {
+      redPaddle.setVelocityY(redPaddleSpeed * -1);
+    } else if (redPaddle.body.position.y < ball.body.position.y) {
+      redPaddle.setVelocityY(redPaddleSpeed);
+    } else {
+      redPaddle.setVelocityY(0);
     }
-
+    // Score
+    if (ball.body.position.x > 770) {
+      resetBall();
+      redScore++;
+      redScoreText.setText('Score: ' + redScore);
+    } else if (ball.body.position.x < 30) {
+      resetBall();
+      blueScore++;
+      blueScoreText.setText('Score: ' + blueScore);
+      redPaddleSpeed += 50;
+    } else {
+      if (redScore === 10 || blueScore === 10) {
+        this.physics.pause();
+      }
+    }
   }
 
-  function handleInput(paddle, up, down) {
-  	if (game.input.keyboard.isDown(up)) {
-  		paddle.body.velocity.y = PADDLE_Vy * -1;
-  	} else if (game.input.keyboard.isDown(down)) {
-  		paddle.body.velocity.y = PADDLE_Vy;
-  	} else {
-  		paddle.body.velocity.y = 0;
-  	}
-  	if (paddle.y < 0) {
-  		paddle.body.position.y = 0;
-  		paddle.body.velocity.y = 0;
-  	} else if (paddle.y > game.world.height - paddle.body.height) {
-  		paddle.body.position.y = game.world.height - paddle.body.height;
-  		paddle.body.velocity.y = 0;
-  	}
+  function resetBall() {
+    ball.setPosition(400, 300);
+    ballDirectionX = (Math.random() > 0.5) ? 1 : -1;
+    ballDirectionY = (Math.random() > 0.5) ? 1 : -1;
+    ballSpeedX = 200 * ballDirectionX;
+    ballSpeedY = 100 * ballDirectionY;
+    ball.setVelocity(ballSpeedX, ballSpeedY);
   }
 
-  function handleCollision(ball, paddle) {
-    // Bounce and speed up the ball
-    var tmpX = ball.body.velocity.x;
-    ball.body.velocity.x = tmpX > 0 ? tmpX + 20 : tmpX - 20;
+  function handleBounce(paddle) {
+    // Increase the ball speed
+    var tmpSpeedX = ball.body.velocity.x;
+    if (tmpSpeedX < 0) {
+      tmpSpeedX -= 50;
+    } else {
+      tmpSpeedX += 50;
+    }
+    ball.setVelocityX(tmpSpeedX);
     // Add some "spin"
-    if (paddle.body.velocity.y > 0) {
-      ball.body.velocity.y += 20;
-    } else if (paddle.body.velocity.y < 0) {
-      ball.body.velocity.y -= 20;
+    var tmpSpeedY = ball.body.velocity.y;
+    if (ball.body.position.y < paddle.body.position.y) {
+      tmpSpeedY -= 50;
+    } else {
+      tmpSpeedY += 50;
     }
+    ball.setVelocityY(tmpSpeedY);
   }
-
 })(Phaser);
